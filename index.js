077bb59830ba131de199562c0a002553bbb0a2c2
Yujeong Christina Yun <yjc.yun@gmail.com>
2020-04-19 14:40:59 -0400

    new design

--- index.js
+++ index.js
@@ -1,51 +1,22 @@
-// home section - view my work button
-let button = document.querySelector('.btn');
-let btnArrow = document.querySelector('#btn-arrow');
-
-button.addEventListener('mouseover', function() {
-   button.children[0].style.transform="rotate(90deg)";
-});
-
-button.addEventListener('mouseout', function() {
-  button.children[0].style.transform="rotate(0deg)";
-});
-
-
-// navbar section - hover effect
-// const setUp = (navItem) => {
-//   let icon = document.querySelector(`#icon-${navItem}`);
-
-//   icon.children[0].addEventListener('mouseover', function() {
-//     icon.children[0].appendChild(document.createTextNode(navItem));
-    
-//   });
-
-//   icon.children[0].addEventListener('mouseout', function() {
-//     icon.children[0].removeChild(icon.children[0].childNodes[0]);
-//   });
-// }
-
-// setUp('home');
-// setUp('profile');
-// setUp('projects');
-// setUp('contact');
-
-// Navbar - hide on home page
-const navbar = document.querySelector('#navbar');
-
-window.onscroll = function() {
-  scrollFunction();
-}
-
-function scrollFunction() {
-  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-    navbar.style.top = '0';
-  } else {
-    navbar.style.top='-500px';
-  }
-}
-
-
-
 //  footer timestamp
 document.querySelector('#current-year').innerHTML = new Date().getFullYear();
+
+//  homepage tilt effect
+$('.intro').tilt({
+  maxTilt: 5,
+  speed: 100,
+})
+
+// homepage hover effect
+const homeLinks = document.querySelectorAll('.intro span');
+const homeH1= document.querySelector('.intro');
+
+homeLinks.forEach(homeLink => {
+  homeLink.addEventListener('mouseover', () => {
+    homeH1.classList.add('greyOut');
+  });
+  homeLink.addEventListener('mouseout', () => {
+    homeH1.classList.remove('greyOut');
+  });
+})
+
